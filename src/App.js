d3873837963d4fcba01e735c821a6619145e00bd
Najeeba143 <najeebanabizada45@gmail.com>
2020-06-25 13:07:40 +0530

    firstproect

--- src/App.js
+++ src/App.js
@@ -2,32 +2,29 @@ import React from 'react';
 // import logo from './logo.svg';
 import './App.css';
 import "./components/style.css";
-import {BrowserRouter ,Link,Switch,Route, Redirect  } from 'react-router-dom';
+import { BrowserRouter as  Switch, Route, Router } from 'react-router-dom';
 // import  { register} from "./components/index";
 import Login from "./components/login";
 import Register from "./components/register"
 import LandingPage from "./components/landingPage"
-const Router = require ("react-router-dom")
-// const Link = require ("react-router-dom").Link;s
-// const gopage = () =>(
-//   <div><Link to="/login">Register</Link></div>
-// )
-// const Approuter = (
-//     <BrowserRouter>
-//       <div>
-//       <Switch>
-//         {/* <Route path="/" exact component={LandingPage} /> */}
-//         <Route   exact component={gopage}/>
-//         {/* <Route path="/login"  component={Login}/> */}
-//       </Switch>
-//       </div>
-//       </BrowserRouter>
-// )
+import history from "./utils/history";
+
+function Approuter() { 
+  return (
+    <Router history={history} >
+      <Switch>
+        <Route path="/" exact component={LandingPage} />
+        <Route path="/register" component={Register} />
+        <Route path="/login"  component={Login} />
+      </Switch>
+      </Router>
+  )
+  }
 
 function App() {
   return (
     <div className="App">
-        <Register/>
+        <Approuter />
     </div>
   );
 }
